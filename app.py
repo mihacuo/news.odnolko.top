80ac50cde2dec6cf17f3b694c1cfc1bf9b09a0b1
Mihhail Odnolko <mihhail.odnolko@gmail.com>
2025-07-16 12:34:35 +0000

    done news

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, g, render_template, flash
 from config import Config
 from forms import SearchForm
+from datetime import datetime, timedelta
+from rss_news_v2 import Rss_en
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +14,21 @@ def index():
     g.form = SearchForm()
     if g.form.validate_on_submit():
         g.search_term = g.form.query.data
+        g.duration = int(g.form.duration.data)
+        end_date = datetime.now()
+        start_date = end_date - timedelta(days=g.duration)
+
         flash(f"{g.search_term=}")
+        flash(f"{g.duration=} days")
+        flash(f"{start_date=}")
+        flash(f"{end_date=}")
+        dated_news = Rss_en.get_news_dated(start=start_date, end=end_date)
+        result_news = Rss_en.get_news(dated_news[0], include=[g.search_term])
+        g.data_news = result_news[0]
+        flashes = dated_news[1]
+        flashes += result_news[1]
+        for _ in flashes:
+            flash(_)
+
 
     return render_template("index.html")
--- forms.py
+++ forms.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
     query = StringField('Search', validators=[DataRequired()])
+    duration = SelectField(
+        'Select Duration',
+        choices=[
+            (1, '1 Day'),
+            (3, '3 Days'),
+            (7, '1 Week'),
+            (14, '2 Weeks'),
+            (30, '1 Month')
+        ],
+        validators=[DataRequired()]
+    )
+
     submit = SubmitField('Go')
